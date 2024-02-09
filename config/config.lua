32daf8ebd66f76fb3fe1ded24acb1850f14b8360
Stoic <112611821+TheStoicBear@users.noreply.github.com>
2024-02-08 18:23:34 -0800

    Update config.lua

--- config/config.lua
+++ config/config.lua
@@ -1,9 +1,5 @@
 Config = {
-    flameSize = 1.5, -- Default flame size is 1.5, anything above 2.5 looks like shit.
-
-    Cars = { "jester", "adder", "a80", "silvia3", "lp700r", "audquattros"}, -- Add cars to the config!
-
-    explosionSpeed = 500, -- I recommend to leave this value at 350. Up to you tho, I cant tell you what to do..
-
-    RPM = 0.85
+    flameSize = 3.5, -- Default flame size is 1.5, anything above 2.5 looks like shit.
+    explosionSpeed = 350, -- I recommend to leave this value at 350. Up to you tho, I cant tell you what to do..
+    RPM = 0.55
 }
