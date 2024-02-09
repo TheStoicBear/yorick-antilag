7488b1e334c93993168aa7c7a58eb15bd72b26b6
Stoic <112611821+TheStoicBear@users.noreply.github.com>
2024-02-08 18:23:14 -0800

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,49 +1,48 @@
 local isAntiLagEnabled = true
 
-RegisterCommand("antilag", function()
-    isAntiLagEnabled = not isAntiLagEnabled
-    PlaySoundFrontend(-1, 'CONFIRM_BEEP', 'HUD_MINI_GAME_SOUNDSET', true)
-    message("~" ..
-        (isAntiLagEnabled and "g" or "r") ..
-        "~Antilag is now " .. (isAntiLagEnabled and "enabled" or "disabled") .. " for you~")
-end, false)
-
 local reverse = 0
 
-CreateThread(function()
-    while true do
-        local sleep = 1000
-        local player = PlayerPedId()
-        local veh = GetVehiclePedIsIn(player, false)
-        local delay = math.random(25, Config.explosionSpeed)
-        if GetPedInVehicleSeat(GetVehiclePedIsIn(PlayerPedId()), -1) == player and isAntiLagEnabled then
-            sleep = 0
-            local RPM = GetVehicleCurrentRpm(veh, player)
-            local gear = GetVehicleCurrentGear(veh)
+function ApplyAntiLagEffects(veh)
+    local player = PlayerPedId()
+    local RPM = GetVehicleCurrentRpm(veh, player)
+    local gear = GetVehicleCurrentGear(veh)
 
-            for _, cars in pairs(Config.Cars) do
-                local vehicleModel = GetEntityModel(veh, player)
-                if GetHashKey(cars) == vehicleModel then
-                    if gear ~= reverse then
-                        if not IsEntityInAir(veh) then
-                            if not IsControlPressed(1, 71) and not IsControlPressed(1, 72) then
-                                if RPM > Config.RPM then
-                                    TriggerServerEvent("flames", VehToNet(veh))
-                                    TriggerServerEvent("sound_server:PlayWithinDistance", 25.0,
-                                        tostring(math.random(1, 6)),
-                                        0.9)
-                                    SetVehicleTurboPressure(veh, 25)
-                                    Wait(delay)
-                                end
-                            end
-                        end
-                    end
+    if gear ~= reverse then
+        if not IsEntityInAir(veh) then
+            if not IsControlPressed(1, 71) and not IsControlPressed(1, 72) then
+                if RPM > Config.RPM then
+                    TriggerServerEvent("flames", VehToNet(veh))
+                    TriggerServerEvent("sound_server:PlayWithinDistance", 25.0,
+                        tostring(math.random(1, 6)),
+                        0.9)
+                    SetVehicleTurboPressure(veh, 25)
+                    Wait(math.random(25, Config.explosionSpeed))
                 end
             end
         end
-        Wait(sleep)
     end
-end)
+end
+
+Citizen.CreateThread(function()
+        while true do
+            local sleep = 1000
+            local player = PlayerPedId()
+            local veh = GetVehiclePedIsIn(player, false)
+
+            local itemExists = exports.ox_inventory:Search('count', 'pop_tune')
+
+            if itemExists > 0 and IsPedInAnyVehicle(player, false) then
+                ApplyAntiLagEffects(veh)
+            end
+
+            Wait(sleep)
+        end
+    end)
+
+
+-- Export the AntiLagThread function
+exports('AntiLagThread', AntiLagThread)
+
 
 local exhausts = { "exhaust", "exhaust_2", "exhaust_3", "exhaust_4" }
 local fxName = "veh_backfire"
